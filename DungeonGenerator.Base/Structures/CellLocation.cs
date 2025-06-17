f874ac28d312ba94d6e2f56e8905834a7cc5d648
Wagner Scholl Lemos <josephblt@gmail.com>
2025-06-17 07:01:44 -0300

    Change name to CellLocation.

---
+++ DungeonGenerator.Base/Structures/CellLocation.cs
@@ -0,0 +1,22 @@
+﻿namespace DungeonGenerator.Base
+{
+    public class CellLocation
+    {
+        #region Constructor
+
+        public CellLocation(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+
+        #region Attributes and Properties
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        #endregion
+    }
+}
