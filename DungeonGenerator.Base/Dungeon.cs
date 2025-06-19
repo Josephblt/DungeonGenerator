9fb8099ffdd68d648da515d0b2d4283d240bcb82
Wagner Scholl Lemos <josephblt@gmail.com>
2025-06-18 22:57:15 -0300

    Adding new Flags.

--- DungeonGenerator.Base/Dungeon.cs
+++ DungeonGenerator.Base/Dungeon.cs
@@ -9,19 +9,31 @@ namespace DungeonGenerator.Base
         [Flags]
         public enum DungeonFlags
         {
-            None = 0,
+            NONE = 0,
 
-            Unused = 1,
-            Path = 2,
-            Room = 4,
+            UNUSED = 1,
+            PATH = 2,
+            ROOM = 4,
 
-            Left = 8,
-            Top = 16,
-            Right = 32,
-            Bottom = 64,
+            LEFT = 8,
+            TOP = 16,
+            RIGHT = 32,
+            BOTTOM = 64,
 
-            Start = 128,
-            Finish = 256
+            START = 128,
+            FINISH = 256,
+            DEADEND = 512,
+
+            TIER1ROOM = 1024,
+            TIER2ROOM = 2048,
+            TIER3ROOM = 4096,
+
+            RED = 8192,
+            CYAN = 16384,
+            GREEN = 32768,
+            MAGENTA = 65536,
+            BLUE = 131072,
+            YELLOW = 262144
         }
 
         #endregion
@@ -48,12 +60,12 @@ namespace DungeonGenerator.Base
             get
             {
                 if ((x < 0) || (x >= Width) || (y < 0) || (y >= Height))
-                    return DungeonFlags.None;
+                    return DungeonFlags.NONE;
 
                 return _grid[x, y];
             }
         }
-        public DungeonFlags this[Cell cellLocation]
+        public DungeonFlags this[CellLocation cellLocation]
         {
             get
             {
@@ -76,7 +88,7 @@ namespace DungeonGenerator.Base
             _grid = new DungeonFlags[Width, Height];
             for (int x = 0; x < Width; x++)
                 for (int y = 0; y < Height; y++)
-                    SetFlag(x, y, DungeonFlags.Unused);
+                    SetFlag(x, y, DungeonFlags.UNUSED);
         }
 
         private void SetFlag(int x, int y, DungeonFlags cellFlag)
@@ -99,74 +111,74 @@ namespace DungeonGenerator.Base
 
         #region Public Methods
 
-        public void SetFlag(Cell cellLocation, DungeonFlags cellFlag)
+        public void SetFlag(CellLocation cellLocation, DungeonFlags cellFlag)
         {
             SetFlag(cellLocation.X, cellLocation.Y, cellFlag);
         }
 
-        public void ClearFlag(Cell cellLocation, DungeonFlags cellFlag)
+        public void ClearFlag(CellLocation cellLocation, DungeonFlags cellFlag)
         {
             ClearFlag(cellLocation.X, cellLocation.Y, cellFlag);
         }
 
-        public void ClearAllFlags(Cell cellLocation)
+        public void ClearAllFlags(CellLocation cellLocation)
         {
-            _grid[cellLocation.X, cellLocation.Y] = DungeonFlags.None;
+            _grid[cellLocation.X, cellLocation.Y] = DungeonFlags.NONE;
         }
 
-        public void ConnectCells(Cell firstCell, Cell secondCell)
+        public void ConnectCells(CellLocation firstCell, CellLocation secondCell)
         {
             var xDirection = firstCell.X - secondCell.X;
             var yDirection = firstCell.Y - secondCell.Y;
 
             if (xDirection == -1)
             {
-                SetFlag(firstCell, DungeonFlags.Right);
-                SetFlag(secondCell, DungeonFlags.Left);
+                SetFlag(firstCell, DungeonFlags.RIGHT);
+                SetFlag(secondCell, DungeonFlags.LEFT);
             }
             if (xDirection == 1)
             {
-                SetFlag(firstCell, DungeonFlags.Left);
-                SetFlag(secondCell, DungeonFlags.Right);
+                SetFlag(firstCell, DungeonFlags.LEFT);
+                SetFlag(secondCell, DungeonFlags.RIGHT);
             }
 
             if (yDirection == -1)
             {
-                SetFlag(firstCell, DungeonFlags.Bottom);
-                SetFlag(secondCell, DungeonFlags.Top);
+                SetFlag(firstCell, DungeonFlags.BOTTOM);
+                SetFlag(secondCell, DungeonFlags.TOP);
             }
             if (yDirection == 1)
             {
-                SetFlag(firstCell, DungeonFlags.Top);
-                SetFlag(secondCell, DungeonFlags.Bottom);
+                SetFlag(firstCell, DungeonFlags.TOP);
+                SetFlag(secondCell, DungeonFlags.BOTTOM);
             }
         }
 
-        public void DisconnectCells(Cell firstCell, Cell secondCell)
+        public void DisconnectCells(CellLocation firstCell, CellLocation secondCell)
         {
             var xDirection = firstCell.X - secondCell.X;
             var yDirection = firstCell.Y - secondCell.Y;
 
             if (xDirection == -1)
             {
-                ClearFlag(firstCell, DungeonFlags.Right);
-                ClearFlag(secondCell, DungeonFlags.Left);
+                ClearFlag(firstCell, DungeonFlags.RIGHT);
+                ClearFlag(secondCell, DungeonFlags.LEFT);
             }
             if (xDirection == 1)
             {
-                ClearFlag(firstCell, DungeonFlags.Left);
-                ClearFlag(secondCell, DungeonFlags.Right);
+                ClearFlag(firstCell, DungeonFlags.LEFT);
+                ClearFlag(secondCell, DungeonFlags.RIGHT);
             }
 
             if (yDirection == -1)
             {
-                ClearFlag(firstCell, DungeonFlags.Bottom);
-                ClearFlag(secondCell, DungeonFlags.Top);
+                ClearFlag(firstCell, DungeonFlags.BOTTOM);
+                ClearFlag(secondCell, DungeonFlags.TOP);
             }
             if (yDirection == 1)
             {
-                ClearFlag(firstCell, DungeonFlags.Top);
-                ClearFlag(secondCell, DungeonFlags.Bottom);
+                ClearFlag(firstCell, DungeonFlags.TOP);
+                ClearFlag(secondCell, DungeonFlags.BOTTOM);
             }
         }
 
