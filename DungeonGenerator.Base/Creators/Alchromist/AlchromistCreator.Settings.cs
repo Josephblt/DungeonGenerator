6a9f36f31645402cc389fbbf735d8ccf69d5b478
Wagner Scholl Lemos <josephblt@gmail.com>
2025-07-07 19:51:27 -0300

    Chamber and tier settings.

--- DungeonGenerator.Base/Creators/Alchromist/AlchromistCreator.Settings.cs
+++ DungeonGenerator.Base/Creators/Alchromist/AlchromistCreator.Settings.cs
@@ -3,49 +3,80 @@
     public partial class AlchromistCreator
     {
         #region Enumerators
-        protected enum Chambers
+
+        public enum Chambers
         {
-            CYAN,
-            RED,
-            MAGENTA,
-            GREEN,
-            YELLOW,
-            BLUE
+            CYAN = 0,
+            RED = 1,
+            MAGENTA = 2,
+            GREEN = 4,
+            YELLOW = 8,
+            BLUE = 16
         }
 
         #endregion
 
         #region Attributes and Properties
 
-        protected int Seed { get; set; }
+        public int Seed { get; set; }
 
-        protected Chambers Chamber { get; set; }
+        public Chambers Chamber { get; set; }
 
-        protected int Size { get; set; }
+        public int Size
+        {
+            get
+            {
+                return (TiersOutterMargin * 4) +
+                       (Tier1CenterSize) + (2 * Tier1MazeSize) + (2 * TiersInnerMargin) +
+                       (Tier2CenterSize) + (2 * Tier2MazeSize) + (2 * TiersInnerMargin) +
+                       (Tier3CenterSize) + (2 * Tier3MazeSize) + (2 * TiersInnerMargin);
+            }
+        }
+        
+        public int TiersOutterMargin { get; set; }
+        public int TiersInnerMargin { get; set; }
 
-        protected CellLocation Start { get; set; }
+        public int Tier1CenterSize { get; set; }
+        public int Tier1MazeSize { get; set; }
+        public int Tier1RoomSize
+        { 
+            get 
+            {
+                return Tier1CenterSize + (2 * Tier1MazeSize) + (2 * TiersInnerMargin);
+            }
+        }
+        
+        public int Tier2CenterSize { get; set; }
+        public int Tier2MazeSize { get; set; }
+        public int Tier2RoomSize
+        {
+            get
+            {
+                return Tier2CenterSize + (2 * Tier2MazeSize) + (2 * TiersInnerMargin);
+            }
+        }
 
-        protected int Tier1RoomSize { get; set; }
-        protected int Tier1MazeMargin { get; set; }
-        protected int Tier1MazeSize { get; set; }
-        protected int Tier2RoomSize { get; set; }
-        protected int Tier2MazeMargin { get; set; }
-        protected int Tier2MazeSize { get; set; }
-        protected int Tier3RoomSize { get; set; }
-        protected int Tier3MazeMargin { get; set; }
-        protected int Tier3MazeSize { get; set; }
 
+        public int Tier3CenterSize { get; set; }
+        public int Tier3MazeSize { get; set; }
+        public int Tier3RoomSize
+        {
+            get
+            {
+                return Tier3CenterSize + (2 * Tier3MazeSize) + (2 * TiersInnerMargin);
+            }
+        }
 
-        protected int RoomQuantity { get; set; }
-        protected int RoomDistance { get; set; }
-        protected int RoomMinSize { get; set; }
-        protected int RoomMaxSize { get; set; }
-        protected int RoomConnections { get; set; }
+        public int RoomQuantity { get; set; }
+        public int RoomDistance { get; set; }
+        public int RoomMinSize { get; set; }
+        public int RoomMaxSize { get; set; }
+        public int RoomConnections { get; set; }
 
-        protected int CorridorBias { get; set; }
+        public int CorridorBias { get; set; }
 
-        protected int DeadEndQuantity { get; set; }
-        protected int DeadEndBias { get; set; }
+        public int DeadEndBias { get; set; }
+        public int DeadEndQuantity { get; set; }
 
         #endregion
     }
